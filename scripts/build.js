11bbbcf0a28083176fd3c050dfc050b3ba0540b8
taotao_glb <taotao_glb@icloud.com>
2022-01-10 17:37:56 +0800

    first commit

---
+++ scripts/build.js
@@ -0,0 +1,17 @@
+const myWebpack = require( '../lib/myWebpack' )
+const config = require( '../config/webpack.config' )
+
+const compiler = myWebpack( config )
+// 开始打包
+compiler.run()
+
+
+
+
+
+
+
+
+
+
+
