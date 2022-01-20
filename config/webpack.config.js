70a979fa07d1a664a52a33d3c66cbeefae8c67cd
王金涛 <wangjintao@wangjintaodeMacBook-Pro.local>
2022-01-20 09:52:21 +0800

    format on save

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require( 'path' )
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve( __dirname, '../dist' ),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'main.js'
   }
 }
